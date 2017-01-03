c99b67f3450ec50c9f0833c9f5837a0dd8397e45
gibran <gibran@entropy.tech>
2017-01-03 11:29:51 -0600

    Add Parallel version of MatrixProd.

--- math.go
+++ math.go
@@ -5,7 +5,7 @@ import (
 	"runtime"
 )
 
-// Vectorprod return the scalar product between two vectors.
+// VectorProduct return the scalar product between two vectors.
 func VectorProduct(x, y []float64) float64 {
 	l := len(x)
 	prod := 0.0000
@@ -18,15 +18,16 @@ func VectorProduct(x, y []float64) float64 {
 	return prod
 }
 
+// MatrixProduct return the product between two matrix
 func MatrixProduct(X, Y [][]float64, c chan [][]float64) {
-	var row []float64
 	var prod [][]float64
 	l1 := len(X)
 	l2 := len(Y)
-	if len(X[0]) != l2 {
+	if len(X[0]) != len(Y[0]) {
 		panic("Matrix dimension error!")
 	}
 	for i := 0; i < l1; i++ {
+		var row []float64
 		for j := 0; j < l2; j++ {
 			row = append(row, VectorProduct(X[i], Y[j]))
 		}
@@ -35,23 +36,24 @@ func MatrixProduct(X, Y [][]float64, c chan [][]float64) {
 	c <- prod
 }
 
+// ParallelMatrixProd compute distribute the product between two matrix
 func ParallelMatrixProd(X, Y [][]float64) (u [][]float64) {
 	l1 := len(X)
-	l2 := len(Y)
-	if len(X[0]) != l2 {
+	if len(X[0]) != len(Y[0]) {
 		panic("Matrix dimension error!")
 	}
-	n := runtime.NumCPU() * 2
+	n := runtime.NumCPU()
 	N := int(math.Floor(float64(l1 / n)))
 	c := make(chan [][]float64)
 	for i := 0; i < n; i++ {
-		j1 := i*N + 1
+		j1 := i * N
 		j2 := (i + 1) * N
 		go MatrixProduct(X[j1:j2], Y, c)
 	}
 
-	if n*N != len(X) {
-		go tMatrixProduct(X[(n*N):(len(X)-1)], Y, c)
+	if n*N != l1 {
+		n++
+		go MatrixProduct(X[(n*N):(l1-1)], Y, c)
 	}
 
 	for i := 0; i < n; i++ {
--- math_test.go
+++ math_test.go
@@ -1,6 +1,9 @@
-package brain
+package math
 
 import (
+	"io/ioutil"
+	"strconv"
+	"strings"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
@@ -9,10 +12,47 @@ import (
 func TestMathFunction(t *testing.T) {
 
 	Convey("Given the following dataset ...", t, func() {
+		var data [][]float64
+		var X [][]float64
+		filePath := "/home/gibran/Work/Go/src/github.com/entropyx/math/dataset/dataset2.txt"
+		strInfo, err := ioutil.ReadFile(filePath)
+		if err != nil {
+			panic(err)
+		}
 
-		Convey(" ...", func() {
-			//coef, _ := GetLogisticCoef(y, X, J)
-			//So(coef, ShouldResemble, []float64{-25.160920349329682, 0.20622792851376404, 0.201468441137889})
+		trainingData := strings.Split(string(strInfo), "\n")
+		for _, line := range trainingData {
+			if line == "" {
+				break
+			}
+
+			var values []float64
+			for _, value := range strings.Split(line, " ") {
+				floatVal, err := strconv.ParseFloat(value, 64)
+				if err != nil {
+					panic(err)
+				}
+				values = append(values, floatVal)
+			}
+			data = append(data, values)
+		}
+
+		for i := 0; i < len(data); i++ {
+			X = append(X, data[i][:3])
+		}
+
+		Theta := [][]float64{make([]float64, len(X[0]))}
+		c := make(chan [][]float64)
+
+		Convey("The product between matrix X an Theta ... ", func() {
+			go MatrixProduct(X, Theta, c)
+			prod := <-c
+			So(len(prod), ShouldEqual, len(X))
+		})
+
+		Convey("The parallel product between matrix X an Theta ... ", func() {
+			prod := ParallelMatrixProd(X, Theta)
+			So(len(prod), ShouldEqual, len(X))
 		})
 
 	})
